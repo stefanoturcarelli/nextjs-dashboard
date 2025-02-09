69eaded6bbed01bf90fba146734e357d9f5dd91a
stefanoturcarelli <stefano>
2025-02-09 16:34:59 -0600

    Remove .env.example file and integrate Vercel Analytics into the layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
-import "@/app/ui/global.css";
-import { inter } from "@/app/ui/fonts";
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function RootLayout({
   children,
@@ -7,8 +8,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
-    </html>
+    <>
+      <Analytics />
+      <html lang="en">
+        <body className={`${inter.className} antialiased`}>{children}</body>
+      </html>
+    </>
   );
 }
